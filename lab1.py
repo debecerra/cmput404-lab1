ce55e2f7a2778783f157f97eb959e5c9a87b3d18
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-09-07 20:10:23 -0600

    second commit

--- lab1.py
+++ lab1.py
@@ -1,11 +1,11 @@
 import requests;
 
 print("requests module version:", requests.__version__)
+print()
 
 """ Ben Lloyd Pearson, How to Use the Python Requests Module With REST APIs
     2020-07-11, https://www.nylas.com/blog/use-python-requests-module-rest-apis/
     Public Domain
 """
-response = requests.get("http://www.google.com")
-print(response)
-
+response = requests.get("https://raw.githubusercontent.com/debecerra/cmput404-lab1/main/lab1.py")
+print(response.text.replace("\\n", "\n"))
